580eb8ed113986fe7cdb9076666b98fe02663d57
estarlyJesus <estarlyjesusamaya@gmail.com>
2022-03-25 17:01:59 -0500

    CREACION
    - comienzo de creacion del item de las peliculas
    - Implementacion de librerias para imagenes

--- Peliculas/app/src/main/java/com/wposs/peliculas/ui/listMovies/ListMoviesFragment.kt
+++ Peliculas/app/src/main/java/com/wposs/peliculas/ui/listMovies/ListMoviesFragment.kt
@@ -19,7 +19,7 @@ class ListMoviesFragment : Fragment() {
     }
 
     private lateinit var listBinding: FragmentListMoviesBinding
-    private val listModel :ListViewModel by viewModels()
+    private          val listModel  : ListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
