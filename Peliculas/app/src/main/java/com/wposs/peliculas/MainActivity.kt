f878d9de8b9e2941153f70e35cac3be36ef30012
estarlyJesus <estarlyjesusamaya@gmail.com>
2022-03-25 14:52:18 -0500

    CREACION
    - Se creo la barra de navegacion
    - Implementacion de databinding

--- Peliculas/app/src/main/java/com/wposs/peliculas/MainActivity.kt
+++ Peliculas/app/src/main/java/com/wposs/peliculas/MainActivity.kt
@@ -2,10 +2,25 @@ package com.wposs.peliculas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.wposs.peliculas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+    fun activeButtonsBar(){
+        mainBinding.btnHome.setOnClickListener {
+
+        }
+        mainBinding.btnFavorites.setOnClickListener {
+
+        }
     }
 }
