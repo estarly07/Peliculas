87f8f895244eea5305be123e52b6f1c97873d983
estarlyJesus <estarlyjesusamaya@gmail.com>
2022-03-25 16:22:01 -0500

    CREACION
    - Creacion de logica de navegacion entre home y favoritos

--- Peliculas/app/src/main/java/com/wposs/peliculas/ui/listMovies/ListMoviesFragment.kt
+++ Peliculas/app/src/main/java/com/wposs/peliculas/ui/listMovies/ListMoviesFragment.kt
@@ -5,32 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.wposs.peliculas.R
+import com.wposs.peliculas.databinding.FragmentListMoviesBinding
 
 
 class ListMoviesFragment : Fragment() {
 
-    companion object{
+    companion object {
 
-      lateinit  var navigation:(page:String )->Unit
+        lateinit var navigation: (page: String) -> Unit
     }
+
+    private lateinit var listBinding: FragmentListMoviesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_movies, container, false)
+        listBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_list_movies, container, false)
+        return listBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigation = {
-            if (it=="HOME"){
-
-            }else{
-
+            if (it == "HOME") {
+                listBinding.home.visibility = View.VISIBLE
+                listBinding.favorites.visibility = View.GONE
+            } else {
+                listBinding.home.visibility = View.GONE
+                listBinding.favorites.visibility = View.VISIBLE
             }
         }
     }
